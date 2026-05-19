5a6c1a437aaa2a9c764bfcff641cc80518d4b562
andoh <solomonkingsman@gmail.com>
2026-05-19 14:37:26 +0000

    refactor core structue, setup app config

---
+++ src/core/config.py
@@ -0,0 +1,21 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    APP_NAME: str = "fast-auth"
+
+    DATABASE_URL: str
+    REDIS_URL: str
+
+    JWT_SECRET_KEY: str
+    JWT_ALGORITHM: str = "HS256"
+
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 15
+    REFRESH_TOKEN_EXPIRE_DAYS: int = 7
+
+    model_config = SettingsConfigDict(
+        env_file=".env", case_sensitive=True, extra="ignore"
+    )
+
+
+settings = Settings()  # type: ignore

8fc7ded1a77bc2ce45f0d2a687a05e772eb70738
andoh <solomonkingsman@gmail.com>
2026-05-23 03:00:52 +0000

    add version to app instance

--- src/__init__.py
+++ src/__init__.py
@@ -1,9 +1,9 @@
 from fastapi import FastAPI
 from src.core.config import settings
 
-app = FastAPI(
-    title=settings.APP_NAME,
-)
+version = settings.VERSION
+
+app = FastAPI(title=settings.APP_NAME, version=version)
 
 
 @app.get("/health")

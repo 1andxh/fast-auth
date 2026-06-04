e3bcdd2ff16de316e2c3d89249946e41f5a0c8b5
andoh <solomonkingsman@gmail.com>
2026-06-04 23:40:40 +0000

    add user registration

--- src/auth/services.py
+++ src/auth/services.py
@@ -1,6 +1,9 @@
 from sqlalchemy.ext.asyncio import AsyncSession
 from dataclasses import dataclass
 import uuid
+from src.users.services import UserService
+from .security import Security
+from src.users import User
 
 
 @dataclass(frozen=True)
@@ -9,8 +12,21 @@ class AuthToken:
     refresh_token: str
 
 
+
+class AuthService:
+    def __init__(self, user_service: UserService, security: Security) -> None:
+        self.user_service = user_service
+        self.security = security
+
+    async def register(self, session: AsyncSession, email: str, password: str) -> User:
+        password_hash = Security.hash_password(password=password)
+        user = await self.user_service.create_user(session=session, email=email, password_hash=password_hash)
+        await session.commit()
+        await session.refresh(user)
+
+        return user
 class SessionService:
-    def __init__(self, session: AsyncSession) -> None:
+    def __init__(self, session: AsyncSession, ) -> None:
         self.session = session
 
     async def create_session(self, user_id: uuid.UUID):

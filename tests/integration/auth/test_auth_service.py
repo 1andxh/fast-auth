91f09fb4eed58e6719dc38d236fbfe32339e6033
andoh <solomonkingsman@gmail.com>
2026-06-06 22:34:00 +0000

    test auth service

--- tests/integration/auth/test_auth_service.py
+++ tests/integration/auth/test_auth_service.py
@@ -1,6 +1,6 @@
 import pytest
 import asyncio
-from src.core.exceptions import DuplicateEmailError
+from src.core.exceptions import DuplicateEmailError, InactiveUserError, InvalidCredentialsError
 
 @pytest.mark.asyncio
 async def test_register_user(db_session, auth_service):
@@ -20,3 +20,28 @@ async def test_duplicate_registration(db_session, auth_service):
     with pytest.raises(DuplicateEmailError):
         await auth_service.register(db_session, email="john@doe.com", password="passpass")
 
+@pytest.mark.asyncio
+async def test_authentication_success(db_session, auth_service):
+    from src.auth.security import security
+    user =  await auth_service.authenticate(db_session, email="john@doe.com", password="pass")
+
+    assert user is not None
+    
+
+@pytest.mark.asyncio
+async def test_authenticate_wrong_password(db_session, auth_service):
+    from src.auth.security import security
+    pass
+
+@pytest.mark.asyncio
+async def test_authenticate_unknown_user(db_session, auth_service):
+    user = await auth_service.authenticate(db_session, email="john@doe.com", password="passpass")
+    with pytest.raises(InvalidCredentialsError):
+        assert user is None
+
+
+@pytest.mark.asyncio
+async def test_authenticate_inactive_user(db_session, auth_service):
+    user = await auth_service.authenticate(db_session, email="john@doe.com", password="passpass")
+    with pytest.raises(InactiveUserError):
+        assert user.is_active == False

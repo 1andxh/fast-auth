35fd6a62f008da2e630920131cb4c279aad7ac28
andoh <solomonkingsman@gmail.com>
2026-06-06 22:33:12 +0000

    add user authentication

--- src/auth/services.py
+++ src/auth/services.py
@@ -4,6 +4,7 @@ import uuid
 from src.users.services import UserService
 from .security import Security
 from src.users import User
+from src.core.exceptions import InvalidCredentialsError, InactiveUserError
 
 
 @dataclass(frozen=True)
@@ -19,13 +20,25 @@ class AuthService:
         self.security = security
 
     async def register(self, session: AsyncSession, email: str, password: str) -> User:
-        password_hash = Security.hash_password(password=password)
+        password_hash = self.security.hash_password(password=password)
         user = await self.user_service.create_user(session=session, email=email, password_hash=password_hash)
         await session.commit()
         await session.refresh(user)
 
         return user
     
+    async def authenticate(self, session: AsyncSession, email: str, password: str) -> User:
+        user = await self.user_service.get_by_email(session, email)
+        if user is None:
+            raise InvalidCredentialsError()
+        valid_password = self.security.verify_password(user.hashed_password, password)
+        if not valid_password:
+            raise InvalidCredentialsError()
+        if not user.is_active:
+            raise InactiveUserError() 
+        return user
+        
+
 
 class SessionService:
     def __init__(self, session: AsyncSession, ) -> None:

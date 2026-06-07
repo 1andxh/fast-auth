72091bac95faee3ed9af53f1282796a9ff11c7b1
andoh <solomonkingsman@gmail.com>
2026-06-07 15:27:38 +0000

    modularize exceptions

---
+++ src/core/exceptions/__init__.py
@@ -0,0 +1,31 @@
+# src/core/exceptions/__init__.py
+
+from .base import FastAuthError
+from .user import UserAlreadyVerified, UserError, UserNotFoundError, DuplicateEmailError
+from .auth import AuthError, InvalidCredentialsError, InactiveUserError, ExpiredTokenError, RefreshTokenReuseError, InvalidTokenError, InvalidTokenTypeError
+from .session import (
+    SessionError,
+    SessionNotFoundError,
+    SessionRevokedError,
+    SessionExpiredError,
+)
+
+
+__all__ = [
+    "FastAuthError",
+    "AuthError",
+    "InvalidCredentialsError",
+    "InactiveUserError",
+    "ExpiredTokenError",
+    "RefreshTokenReuseError",
+    "InvalidTokenError",
+    "InvalidTokenTypeError",
+    "SessionError",
+    "SessionNotFoundError",
+    "SessionRevokedError",
+    "SessionExpiredError",
+    "UserError",
+    "UserAlreadyVerified",
+    "UserNotFoundError",
+    "DuplicateEmailError"
+]

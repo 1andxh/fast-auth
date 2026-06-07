f139f506a065e847850c29ac063ed18983d658f6
andoh <solomonkingsman@gmail.com>
2026-06-07 15:48:49 +0000

    add session service

--- src/auth/services.py
+++ src/auth/services.py
@@ -4,13 +4,15 @@ import uuid
 from src.users.services import UserService
 from .security import Security
 from src.users import User
-from src.core.exceptions import InvalidCredentialsError, InactiveUserError
+from src.core.exceptions import InvalidCredentialsError, InactiveUserError, SessionRevokedError, SessionExpiredError
+from .models import UserSession
+from src.core.config import settings
+from datetime import datetime, timezone, timedelta
 
-
-@dataclass(frozen=True)
-class AuthToken:
-    access_token: str
-    refresh_token: str
+# @dataclass(frozen=True)
+# class AuthToken:
+#     access_token: str
+#     refresh_token: str
 
 
 
@@ -41,8 +43,36 @@ class AuthService:
 
 
 class SessionService:
+    SESSION_LIFETIME = settings.SESSION_LIFETIME_DAYS
+
     def __init__(self, session: AsyncSession, ) -> None:
         self.session = session
 
-    async def create_session(self, user_id: uuid.UUID):
-        pass
+    async def create_session(self, user_id: uuid.UUID, user_agent: str | None = None, ip_address: str | None = None) -> UserSession:
+        expires_at = datetime.now(timezone.utc) + timedelta(days=self.SESSION_LIFETIME)
+        new_session = UserSession(
+            user_id=user_id, user_agent=user_agent, ip_address=ip_address, expires_at=expires_at
+        )
+        self.session.add(new_session)
+
+        await self.session.flush()
+        return new_session
+
+    async def get_session_by_id(self, session_id: uuid.UUID) -> UserSession | None:
+        return await self.session.get(UserSession,session_id)
+    
+    async def revoke_session(self, user_session: UserSession) -> None:
+        user_session.revoked_at = datetime.now(timezone.utc)
+        await self.session.flush()
+
+    async def validate_session(self, user_session: UserSession) -> bool:
+        now = datetime.now(timezone.utc)
+        if user_session.revoked_at is not None:
+            raise SessionRevokedError()
+        if now >=  user_session.expires_at:
+            raise SessionExpiredError()
+        
+        # slide session window if user is active ?
+        return True
+        
+

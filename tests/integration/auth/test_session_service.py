5c092f434632eaca8e5059faea6145a8af442621
andoh <solomonkingsman@gmail.com>
2026-06-07 17:07:26 +0000

    test get session id

--- tests/integration/auth/test_session_service.py
+++ tests/integration/auth/test_session_service.py
@@ -3,6 +3,7 @@ import asyncio
 from src.core.exceptions import SessionExpiredError, SessionRevokedError
 import uuid
 from src.users import User
+from src.auth.models import UserSession
 
 @pytest.mark.asyncio
 async def test_create_session_succes(db_session,session_service):
@@ -15,4 +16,15 @@ async def test_create_session_succes(db_session,session_service):
     assert session.id is not None
     assert session.user_id == test_user.id
     assert session.revoked_at is None
+
+@pytest.mark.asyncio
+async def test_get_session_by_id(db_session, session_service):
+    session = UserSession(user_id=uuid.uuid4(), user_agent="mozilla/5.0", ip_address='127.0.01')
+    db_session.add(session)
+    await db_session.commit()
+
+    found_session = session_service.get_session_by_id(session.id)
+    assert found_session is not None
+    assert found_session.id == session.id
+
 

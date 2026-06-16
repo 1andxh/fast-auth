a4e2aba9e56da3934e4ad827e17b7acf8509689e
andoh <solomonkingsman@gmail.com>
2026-06-16 21:19:39 +0000

    add user and session dependency

--- src/auth/dependecies.py
+++ src/auth/dependecies.py
@@ -3,36 +3,44 @@ from fastapi.security import HTTPAuthorizationCredentials, HTTPBearer
 from src.db.session import get_session
 from sqlalchemy.ext.asyncio import AsyncSession
 from src.users import User
-from src.users.services import UserService as user_service
+from src.users.services import UserService 
 from .schemas import TokenPayload
 from .services import SessionService, TokenService
 from .models import UserSession
 from .utils import validate_access_token
-from src.core.exceptions import InvalidTokenError, ExpiredTokenError
+from src.core.exceptions import InvalidTokenError, SessionNotFoundError, UserError
 
 security =  HTTPBearer()
+user_service = UserService()
+
+# service dependencies
+async def get_session_service(session: AsyncSession = Depends(get_session)) -> SessionService:
+    return SessionService(session)
+
+
 async def get_current_token(credentials: HTTPAuthorizationCredentials =  Depends(security)) -> TokenPayload:
 
     token = credentials.credentials
     payload = validate_access_token(token=token)
-
     if not payload:
         raise InvalidTokenError()
-    
+  
     return payload
 
 
-async def get_current_session(service: SessionService, payload: TokenPayload = Depends(get_current_token)) -> UserSession:
+async def get_current_session(service: SessionService = Depends(get_session_service), payload: TokenPayload = Depends(get_current_token)) -> UserSession:
     
     session = await service.get_session_by_id(payload.sid)
-    # if session:
-    await service.validate_session(session) if session else None
+    if not session:
+        raise SessionNotFoundError()
     
-    
+    await service.validate_session(session)
+    return session
 
 
+async def get_current_user(session: AsyncSession = Depends(get_session), user_session: UserSession = Depends(get_current_session), ) -> User:
+    user = await user_service.get_by_id(session=session, id=user_session.id)
+    if not user or not user.is_active:
+        raise UserError("User account is diasabled or missing")
 
-
-async def get_current_user(session: UserSession = Depends(get_current_session)) -> User:
-    pass
-
+    return user

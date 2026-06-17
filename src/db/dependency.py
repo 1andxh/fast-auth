47598b60e63a010488659c4083443655869c73d6
andoh <solomonkingsman@gmail.com>
2026-06-17 12:38:58 +0000

    add session dependency

---
+++ src/db/dependency.py
@@ -0,0 +1,6 @@
+from typing import Annotated
+from .session import get_session
+from sqlalchemy.ext.asyncio import AsyncSession
+from fastapi import Depends
+
+session = Annotated[AsyncSession, Depends(get_session)]

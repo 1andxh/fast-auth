6aa9e51a80cfa2902f4773e96fae770decec513e
andoh <solomonkingsman@gmail.com>
2026-06-18 03:42:12 +0000

    add user registration route

--- src/auth/routes.py
+++ src/auth/routes.py
@@ -1,10 +1,26 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, status
 from src.users.schemas import UserResponse
 from src.users.models import User
-from .dependecies import get_current_user
+from .schemas import RegisterRequest, LoginRequest, TokenResponse, TokenPayload
+from .dependecies import get_current_user, get_auth_service
+from .services import AuthService, TokenService
+from typing import Annotated
 
-auth_router = APIRouter()
+auth_router = APIRouter(prefix="/auth")
+service = Annotated[AuthService, Depends(get_auth_service)]
+
 
 @auth_router.get("/me", response_model=UserResponse)
 async def get_user(current_user: User = Depends(get_current_user)) -> User:
     return current_user
+
+@auth_router.post("/register", response_model=UserResponse, status_code=status.HTTP_201_CREATED)
+async def register(payload: RegisterRequest, service: service):
+    user = await service.register(email=payload.email, password=payload.password)
+
+    return user
+
+@auth_router.post("/login", response_model=TokenResponse)
+async def login(payload: LoginRequest, service: service, token_service: TokenService):
+    user = await service.authenticate(email=payload.email, password=payload.password)
+    tokens = await token_service.issue_token_pair(user=user)

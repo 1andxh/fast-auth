5db5c7435434617ea79aa793e6bb6fddbe724279
andoh <solomonkingsman@gmail.com>
2026-06-23 02:56:11 +0000

    pass session in user service constuctor

--- src/users/services.py
+++ src/users/services.py
@@ -8,26 +8,28 @@ from src.core.exceptions import DuplicateEmailError
 
 
 class UserService:
+    def __init__(self, session: AsyncSession) -> None:
+        self.session =  session
 
-    async def get_by_email(self, session: AsyncSession, email: str) -> User | None:
+    async def get_by_email(self, email: str) -> User | None:
         normalized_email = normalize_email(email)
-        stmt = await session.execute(select(User).where(User.email == normalized_email))
+        stmt = await self.session.execute(select(User).where(User.email == normalized_email))
         return stmt.scalar_one_or_none()
 
-    async def get_by_id(self, session: AsyncSession, id: uuid.UUID) -> User | None:
-        return await session.get(User, id)
+    async def get_by_id(self, id: uuid.UUID) -> User | None:
+        return await self.session.get(User, id)
 
     async def create_user(
-        self, session: AsyncSession, email: str, password_hash: str
+        self, email: str, password_hash: str
     ) -> User:
         email = normalize_email(email)
-        existing_user = await self.get_by_email(session, email)
+        existing_user = await self.get_by_email(email)
         if existing_user:
             raise DuplicateEmailError()
         new_user = User(email=email, hashed_password=password_hash)
-        session.add(new_user)
+        self.session.add(new_user)
         try:
-            await session.flush()
+            await self.session.flush()
         except IntegrityError as exc:
             raise DuplicateEmailError() from exc
         return new_user

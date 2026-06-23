daf3a621c35d7a82f3d89eecefc46a3dab7f92bf
andoh <solomonkingsman@gmail.com>
2026-06-23 03:44:47 +0000

    group all auth dependencies

--- src/auth/__init__.py
+++ src/auth/__init__.py
@@ -1,3 +1,6 @@
 from .models import UserSession, RefreshToken
+from .services import RefreshTokenService, AuthService, SessionService, TokenService
+from .security import Security, get_security
+from .dependecies import get_auth_service, get_refresh_service, get_session_service, get_token_service
 
-__all__ = ["UserSession", "RefreshToken"]
+__all__ = ["UserSession", "RefreshToken", "RefreshTokenService", "AuthService" , "SessionService", "TokenService", "Security", "get_security", "get_token_service", "get_auth_service", "get_refresh_service", "get_session_service"]
--- src/auth/dependecies.py
+++ src/auth/dependecies.py
@@ -1,46 +1,39 @@
 from fastapi import Depends, HTTPException
 from fastapi.security import HTTPAuthorizationCredentials, HTTPBearer
-from src.db.session import get_session
-from src.db.dependency import session
-from sqlalchemy.ext.asyncio import AsyncSession
+from src.db.dependency import DbSession
 from src.users import User
-from src.users.services import UserService 
 from .schemas import TokenPayload
 from .services import SessionService, AuthService, TokenService, RefreshTokenService
 from .models import UserSession
 from .utils import validate_access_token
-from .security import Security
 from src.core.exceptions import InvalidTokenError, SessionNotFoundError, UserError
-from typing import Annotated
+from src.core.annotations import SecurityDep, SessionServDep, AuthServDep, UserServDep, RefreshServDep, TokenServDep
 
-security =  HTTPBearer()
-# SessionServDep = 
-# DbSession = 
-# RefreshServDep = 
-# UserServDep
 
-# service dependencies
-async def get_session_service(session: session) -> SessionService:
+http_security =  HTTPBearer()
+
+
+async def get_session_service(session: DbSession) -> SessionService:
     return SessionService(session)
 
-async def get_auth_service(session: session, auth_security: Security, user_service: UserService = Depends(UserService),) -> AuthService:
-    return AuthService(session, user_service)
+async def get_auth_service(session: DbSession, security: SecurityDep, user_service: UserServDep) -> AuthService:
+    return AuthService(session, user_service, security)
 
-async def get_refresh_service(session: session, user_session):
-    pass
+async def get_refresh_service(session: DbSession, security: SecurityDep, service: SessionServDep) -> RefreshTokenService:
+    return RefreshTokenService(session, service, security)
 
-async def get_token_service(session: session, user_session: SessionService, refresh_service: RefreshTokenService, auth_security: Security) -> TokenService:
-    return TokenService(session, user_session, refresh_service,)
+async def get_token_service(session: DbSession, user_session: SessionServDep, refresh_service: RefreshServDep, security: SecurityDep) -> TokenService:
+    return TokenService(session, user_session, refresh_service, security)
 
 
-async def get_current_token(credentials: HTTPAuthorizationCredentials =  Depends(security)) -> TokenPayload:
+async def get_current_token(credentials: HTTPAuthorizationCredentials =  Depends(http_security)) -> TokenPayload:
 
     token = credentials.credentials
     payload = validate_access_token(token=token)  
     return payload
 
 
-async def get_current_session(service: SessionService = Depends(get_session_service), payload: TokenPayload = Depends(get_current_token)) -> UserSession:
+async def get_current_session(service: SessionServDep, payload: TokenPayload = Depends(get_current_token)) -> UserSession:
     
     session = await service.get_session_by_id(payload.sid)
     if not session:
@@ -50,8 +43,8 @@ async def get_current_session(service: SessionService = Depends(get_session_serv
     return session
 
 
-async def get_current_user(session: AsyncSession = Depends(get_session), user_session: UserSession = Depends(get_current_session), service: UserService = Depends(UserService)) -> User:
-    user = await service.get_by_id(session=session, id=user_session.user_id)
+async def get_current_user(service: UserServDep, user_session: UserSession = Depends(get_current_session), ) -> User:
+    user = await service.get_by_id(id=user_session.user_id)
     if not user or not user.is_active:
         raise UserError("User account is diasabled or missing")
 

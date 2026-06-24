97ecaef8eeb70ed024ba9f944f757e695d742db7
andoh <solomonkingsman@gmail.com>
2026-06-24 23:10:33 +0000

    correct wrong url paths

--- tests/integration/auth/test_auth_routes.py
+++ tests/integration/auth/test_auth_routes.py
@@ -1,17 +1,23 @@
 import pytest
+from tests.conftest import API_PREFIX
 
-from src.core.config import settings
 
+@pytest.mark.asyncio
+async def test_get_current_user(client, create_test_user):
+    user = await create_test_user()
+    login = await client.post(f"{API_PREFIX}login", json={"email": user.email, "password": "password"})
+    token = login.json()["access_token"]
 
-# @pytest.mark.asyncio
-# async def test_get_current_user(client, create_test_user):
-#     user = await create_test_user()
-#     token = client.post("/auth")
+    response = await client.get(f"{API_PREFIX}/me", headers={"Authorization": f"Bearer {token}"})
+    assert response.status_code == 200
+
+    body =  response.json()
+    assert body["email"] == user.email
 
 
 @pytest.mark.asyncio
 async def tests_register_success(client):
-    response = await client.post("/auth/register", json={"email":"test1@email.com", "password":"testpass2121"})
+    response = await client.post(f"{API_PREFIX}/register", json={"email":"test1@email.com", "password":"testpass2121"})
     assert response.status_code == 201
     
     body = response.json()
@@ -20,7 +26,9 @@ async def tests_register_success(client):
 @pytest.mark.asyncio
 async def test_login_success(client, create_test_user):
     user = await create_test_user()
-    response = await client.post("/auth/login", json={"email": user.email, "password": "password"})
+    response = await client.post(f"{API_PREFIX}/login", json={"email": user.email, "password": "password"})
+
+    print(response.status_code, response.json())
     
     assert response.status_code == 200
     

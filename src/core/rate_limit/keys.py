67a12f879fc7d8880691f4efcde741dd470e63d3
andoh <solomonkingsman@gmail.com>
2026-06-29 03:45:23 +0000

    write key function to get client ip

---
+++ src/core/rate_limit/keys.py
@@ -0,0 +1,13 @@
+from fastapi import Request
+
+
+async def ip_key(request: Request) -> str:
+
+    forwarded = request.headers.get("X-Forwarded-FOR")
+    if forwarded:
+        return forwarded.split(",")[0].strip()
+
+    if request.client is None:
+        return "Unknown"
+
+    return request.client.host

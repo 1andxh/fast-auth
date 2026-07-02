9475d7dea820f74b7bcaa25e0f1a0f9c967d19d2
andoh <solomonkingsman@gmail.com>
2026-07-02 16:07:33 +0000

    log rate limit on exception raise

--- src/core/rate_limit/integration.py
+++ src/core/rate_limit/integration.py
@@ -1,8 +1,14 @@
-from fastapi import Depends, Request, Response, HTTPException
+from fastapi import Depends, Request, Response
 from fastapicap import Cap, RateLimiter
+
 from src.core.config import settings
 from src.core.logging.logger import logger
-from .policies import Ratelimit, RateLimitPolicies
+from src.core.rate_limit.policies import (
+    Ratelimit,
+    LOGIN_POLICY,
+    REFRESH_POLICY,
+    REGISTER_POLICY,
+)
 from src.core.exceptions.rate_limit import RateLimitExceededError
 
 cap = Cap.init_app(settings.REDIS_URL)
@@ -12,17 +18,14 @@ async def _on_rate_limit(
     request: Request, response: Response, retry_after: int
 ) -> None:
     logger.warning(
-        "rate_limit_exceeeded", path=request.url.path, retry_after=retry_after
+        "rate_limit_exceeeded",
+        path=request.url.path,
+        method=request.method,
+        retry_after=retry_after,
+        client=request.client.host if request.client else None,
     )
 
-    raise HTTPException(
-        status_code=RateLimitExceededError.status_code,
-        detail={
-            "error_code": RateLimitExceededError.error_code,
-            "message": f"Too many requests. Try again in {retry_after} seconds",
-        },
-        headers={"Retry-After": str(retry_after)},
-    )
+    raise RateLimitExceededError(retry_after=retry_after)
 
 
 def _create_rate_limiter(policy: Ratelimit) -> RateLimiter:
@@ -41,3 +44,8 @@ def _create_dependency(policy: Ratelimit):
     limiter = _create_rate_limiter(policy)
 
     return Depends(limiter)
+
+
+LOGIN_RATE_LIMIT = _create_dependency(LOGIN_POLICY)
+REGISTER_RATE_LIMIT = _create_dependency(REGISTER_POLICY)
+REFRESH_RATE_LIMIT = _create_dependency(REFRESH_POLICY)

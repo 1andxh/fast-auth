f5e3f758177ffa03bfa88f50c4290169ebb7466a
andoh <solomonkingsman@gmail.com>
2026-07-06 02:47:35 +0000

    refactor auth service

---
+++ src/auth/services/auth.py
@@ -0,0 +1,48 @@
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from src.core.exceptions import (
+    InactiveUserError,
+    InvalidCredentialsError,
+)
+from src.core.logging.logger import logger
+from src.auth.models import User
+from src.auth.services import UserService
+
+from src.auth.security import Security
+
+
+class AuthService:
+    def __init__(
+        self, session: AsyncSession, user_service: UserService, security: Security
+    ) -> None:
+        self.user_service = user_service
+        self.session = session
+        self.security = security
+
+    async def register(self, email: str, password: str) -> User:
+        password_hash = self.security.hash_password(password=password)
+        user = await self.user_service.create_user(
+            email=email, password_hash=password_hash
+        )
+        await self.session.commit()
+        await self.session.refresh(user)
+
+        logger.info("user_registered", user_id=str(user.id), email=user.email)
+        return user
+
+    async def authenticate(self, email: str, password: str) -> User:
+        user = await self.user_service.get_by_email(email)
+        if user is None:
+            logger.warning("user_login_failed", email=email, reason="user_not_found")
+            raise InvalidCredentialsError()
+        valid_password = self.security.verify_password(user.hashed_password, password)
+        if not valid_password:
+            logger.info("user_login_failed", email=email, reason="invalid_password")
+            raise InvalidCredentialsError()
+        if not user.is_active:
+            logger.warning(
+                "user_login_failed", user_id=str(user.id), reason="inactive_user"
+            )
+            raise InactiveUserError()
+        logger.info("user_login_success", user_id=str(user.id))
+        return user

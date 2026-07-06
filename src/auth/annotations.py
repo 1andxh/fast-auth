724d6906fa54b8763fa5f3a82fbb94f16f802320
andoh <solomonkingsman@gmail.com>
2026-07-06 02:32:05 +0000

    remove user service dependency

--- src/auth/annotations.py
+++ src/auth/annotations.py
@@ -3,7 +3,5 @@ from typing import Annotated
 from fastapi import Depends
 
 from src.auth import Security, get_security
-from src.users import UserService, get_user_service
 
-UserServDep = Annotated[UserService, Depends(get_user_service)]
 SecurityDep = Annotated[Security, Depends(get_security)]
